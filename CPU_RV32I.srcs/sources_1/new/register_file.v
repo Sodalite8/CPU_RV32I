0896502e852d31580ad2361cc0c8a9e39c66a313
Sodalite8 <08sodium.lithium@gmail.com>
2025-02-16 15:42:37 +0900

    register_file

---
+++ CPU_RV32I.srcs/sources_1/new/register_file.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/16/2025 03:37:22 PM
+// Design Name: 
+// Module Name: register_file
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module register_file(
+    input           CLK, 
+    input   [4:0]   rs1, rs2, rd, 
+    input           write, 
+    input   [31:0]  data_in, 
+    output  [31:0]  data_out1, data_out2
+);
+    reg [31:0]  register[31:0];
+    
+    
+    assign data_out1 = (rs1 == 5'h0) ? 32'h0 : register[rs1];
+    assign data_out2 = (rs2 == 5'h0) ? 32'h0 : register[rs2];
+    
+    always @(posedge CLK) begin
+        if(write) begin
+            register[rd] <= data_in;
+        end
+    end
+endmodule
