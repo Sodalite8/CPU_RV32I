26a9bb3fc58979e28b74c08adfe4a8be512539f0
Sodalite8 <08sodium.lithium@gmail.com>
2025-02-17 16:29:12 +0900

    PC_selector実装

---
+++ CPU_RV32I.srcs/sim_1/new/PC_selector.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/17/2025 04:26:48 PM
+// Design Name: 
+// Module Name: PC_selector
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PC_selector(
+    input           PC_source,
+    input   [31:0]  PC_plus_four, PC_adder_out,
+    output  [31:0]  PC_next
+);
+
+
+    assign PC_next = (PC_source) ? PC_adder_out : PC_plus_four;
+endmodule
