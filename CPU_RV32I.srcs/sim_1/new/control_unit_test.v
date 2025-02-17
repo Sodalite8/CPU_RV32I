7d797d7f4a68abf7f7c95f1da6e07b17c10dcccc
Sodalite8 <08sodium.lithium@gmail.com>
2025-02-17 14:26:37 +0900

    control_unit_testテスト内容追加

--- CPU_RV32I.srcs/sim_1/new/control_unit_test.v
+++ CPU_RV32I.srcs/sim_1/new/control_unit_test.v
@@ -30,9 +30,9 @@ module control_unit_test();
     wire    [1:0]   ALU_source2;
     wire            PC_adder_source1;
     wire            inst_or_data, mem_read, mem_write, ALU_or_mem;
-    wire            is_decode;
+    wire            is_decode, is_branch, is_jump;
     
-    parameter       CLK_PERIOD = 100;
+    parameter       CLK_PERIOD = 10;
 
 
     control_unit control_unit(
@@ -49,7 +49,9 @@ module control_unit_test();
         .mem_read(mem_read),
         .mem_write(mem_write),
         .ALU_or_mem(ALU_or_mem),
-        .is_decode(is_decode)
+        .is_decode(is_decode),
+        .is_branch(is_branch),
+        .is_jump(is_jump)
     );
     
     
@@ -67,7 +69,14 @@ module control_unit_test();
         #(CLK_PERIOD)       RES = 1'b0;
                             opcode = 7'b0110011;
         #(CLK_PERIOD)       RES = 1'b1;
-        #(CLK_PERIOD * 3)   opcode = 7'b0010011;    
+        #(CLK_PERIOD * 3)   opcode = 7'b0010011;
+        #(CLK_PERIOD * 4)   opcode = 7'b1100111;
+        #(CLK_PERIOD * 4)   opcode = 7'b1101111;
+        #(CLK_PERIOD * 4)   opcode = 7'b0000011;
+        #(CLK_PERIOD * 5)   opcode = 7'b0100011;
+        #(CLK_PERIOD * 4)   opcode = 7'b1100011;
+        #(CLK_PERIOD * 3)   opcode = 7'b0110111;
+        #(CLK_PERIOD * 4)   opcode = 7'b0010111;
         #(CLK_PERIOD * 4)   $finish;
     end
 endmodule
